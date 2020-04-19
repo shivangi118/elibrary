69bb5c8b63c3a1182e74003fc2b69d021ea8f45e
shivangi118 <shivangi11894@gmail.com>
2020-04-20 00:44:51 +0100

    Third Commit

--- src/com/elibrary/beans/Encrypt.java
+++ src/com/elibrary/beans/Encrypt.java
@@ -1,9 +1,6 @@
 package com.elibrary.beans;
 import org.mindrot.jbcrypt.BCrypt;
 
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
-
 public class Encrypt {
 public static void main(String args[]) {
 	String hah = BCrypt.hashpw("Landmark@202", BCrypt.gensalt());
