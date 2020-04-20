84a300084844388c84c1d1bcc860fb18881edc03
shivangi118 <shivangi11894@gmail.com>
2020-04-20 15:00:53 +0100

    Added webapprunner jar in pom.xml and production db url

--- src/com/elibrary/dao/DB.java
+++ src/com/elibrary/dao/DB.java
@@ -12,7 +12,8 @@ public static Connection getCon(){
 		//Class.forName("oracle.jdbc.driver.OracleDriver");
 		Class.forName("com.mysql.jdbc.Driver");
 		//con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/elibrary","root","root");
+		//con=DriverManager.getConnection("jdbc:mysql://localhost:3306/elibrary","root","root");
+		con=DriverManager.getConnection("mysql://letav0u0mx45zir6:fyev1nxm4s0n6juu@zwgaqwfn759tj79r.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/pggjq3jjkr2nkpl3");
 		
 	}catch(Exception e){System.out.println(e);}
 	return con;
